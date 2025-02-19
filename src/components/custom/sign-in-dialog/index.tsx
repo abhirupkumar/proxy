aa88abc56fa4fe0deb9f2814cfe16763de80761a
abhirupkumar <abhirupkumar2003@gmail.com>
2025-02-19 20:48:18 +0530

    Implement user management with Convex; add user schema, API functions, and authentication middleware

--- src/components/custom/sign-in-dialog/index.tsx
+++ src/components/custom/sign-in-dialog/index.tsx
@@ -4,24 +4,37 @@ import { useUserDetail } from '@/context/UserDetailContext'
 import Lookup from '@/data/Lookup'
 import { useGoogleLogin } from '@react-oauth/google'
 import axios from 'axios'
+import { useMutation } from 'convex/react'
 import React from 'react'
+import { api } from '../../../../convex/_generated/api'
+import { v4 as uuid4 } from 'uuid'
 
 const SignInDialog = ({ openDialog, closeDialog }: { openDialog: boolean, closeDialog: () => void }) => {
 
     const { userDetail, setUserDetail } = useUserDetail();
+    const CreateUser = useMutation(api.users.CreateUser);
 
     const googleLogin = useGoogleLogin({
         onSuccess: async (tokenResponse) => {
             console.log(tokenResponse);
             const userInfo = await axios.get(
                 'https://www.googleapis.com/oauth2/v3/userinfo',
-                { headers: { Authorization: 'Bearer >' + tokenResponse?.access_token } },
+                { headers: { Authorization: 'Bearer ' + tokenResponse?.access_token } },
             );
-
+            const user = userInfo?.data;
+            await CreateUser({
+                name: user.name,
+                email: user.email,
+                picture: user.picture,
+                uid: uuid4()
+            })
+            if (typeof window !== undefined) {
+                localStorage.setItem('user', JSON.stringify(user));
+            }
             setUserDetail(userInfo?.data);
             closeDialog();
         },
-        onError: errorResponse => console.log(errorResponse),
+        onError: errorResponse => console.log("errorResponse: ", errorResponse),
     });
 
     return (
--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,20 +1,44 @@
 'use client'
 
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { MessagesProvider } from '@/context/MessagesContext'
 import { UserDetailProvider } from '@/context/UserDetailContext'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { api } from '../../convex/_generated/api'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export function ThemeProvider({
     children,
     ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-    return <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_KEY!}>.
-        <UserDetailProvider>
-            <MessagesProvider>
-                <NextThemesProvider {...props}>{children}</NextThemesProvider>
-            </MessagesProvider>
-        </UserDetailProvider>
-    </GoogleOAuthProvider>
+
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+    useEffect(() => {
+        IsAuthenticated();
+    }, []);
+
+    const IsAuthenticated = async () => {
+        if (typeof window !== 'undefined') {
+            const user = JSON.parse(localStorage.getItem('user') ?? "");
+            const result = await convex.query(api.users.GetUser, {
+                email: user?.email
+            })
+            console.log(result);
+        }
+    }
+
+    return <ClerkProvider>
+        <ConvexProvider client={convex}>
+            <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_KEY!}>.
+                <UserDetailProvider>
+                    <MessagesProvider>
+                        <NextThemesProvider {...props}>{children}</NextThemesProvider>
+                    </MessagesProvider>
+                </UserDetailProvider>
+            </GoogleOAuthProvider>
+        </ConvexProvider>
+    </ClerkProvider>
 }
