fe3b0e8d19958eaa4c6ff5990ed2003036f64d75
abhirupkumar <abhirupkumar2003@gmail.com>
2025-02-21 23:56:45 +0530

    Add AI chat functionality; implement POST route for AI responses, enhance ChatView component with loading state and user input handling

---
+++ src/app/api/ai-chat/route.ts
@@ -0,0 +1,40 @@
+import Prompt from '@/data/Prompt';
+import { groq } from '@/lib/groq';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    const { prompt, systemPrompt } = await req.json();
+
+    const messages = [
+        {
+            role: "system",
+            content: systemPrompt
+        },
+        ...prompt
+    ];
+
+    try {
+        const stream = await groq.chat.completions.create({
+            model: 'qwen-2.5-coder-32b',
+            messages: messages,
+            stream: true
+        })
+        const encoder = new TextEncoder();
+        const readableStream = new ReadableStream({
+            async start(controller) {
+                for await (const chunk of stream) {
+                    controller.enqueue(encoder.encode(chunk.choices[0]?.delta?.content || ""));
+                }
+                controller.close();
+            },
+        });
+
+        return new NextResponse(readableStream, {
+            headers: { "Content-Type": "text/plain" },
+        });
+    }
+    catch (error: any) {
+        console.log(error)
+        return NextResponse.json({ error: error }, { status: 500 });
+    }
+}
--- src/components/custom/chatview/index.tsx
+++ src/components/custom/chatview/index.tsx
@@ -4,43 +4,95 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { useMessages } from '@/context/MessagesContext';
 import Colors from '@/data/Colors';
 import Lookup from '@/data/Lookup';
+import Prompt from '@/data/Prompt';
 import { useUser } from '@clerk/nextjs';
 import { Workspace } from '@prisma/client';
-import { ArrowRight, Link } from 'lucide-react';
+import { ArrowRight, Bot, Link, Loader2 } from 'lucide-react';
 import Image from 'next/image';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Message } from 'postcss';
 
 const ChatView = ({ workspace }: { workspace: Workspace }) => {
     const { messages, setMessages } = useMessages();
-    const [userInput, setUserInput] = React.useState<string>('');
+    const [userInput, setUserInput] = useState<string>('');
     const { isSignedIn, user, isLoaded } = useUser();
+    const [loading, setLoading] = useState<boolean>(false)
 
     useEffect(() => {
         setMessages(workspace.message);
     }, [workspace.message]);
 
-    const onGenerate = (content: string) => {
+    useEffect(() => {
+        if (messages?.length > 0) {
+            const role = messages[messages?.length - 1].role;
+            if (role == 'user')
+                getAiResponse();
+        }
+    }, [messages]);
 
+    const onGenerate = (content: string) => {
+        setMessages((prev: Message[]) => [...prev, {
+            role: 'user',
+            content: content
+        }])
+        setUserInput('');
+    }
+
+    const getAiResponse = async () => {
+        setLoading(true);
+        const response: any = await fetch('/api/ai-chat', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                prompt: messages,
+                systemPrompt: Prompt.CHAT_PROMPT
+            }),
+        });
+        if (!response.body) return;
+
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder();
+        let botMessage = "";
+
+        while (true) {
+            const { done, value } = await reader.read();
+            if (done) break;
+            setLoading(false);
+
+            botMessage += decoder.decode(value, { stream: true });
+
+            setMessages((prev: any) => [
+                ...(prev[prev.length - 1]?.role == 'ai' ? prev.slice(0, prev.length - 1) : prev),
+                { role: "ai", content: botMessage },
+            ]);
+        }
     }
 
     return (
         <div className='relative h-[85vh] flex flex-col'>
             <div className='flex-1 overflow-y-scroll no-scrollbar'>
                 {messages?.map((message: any, index: number) => (
-                    <div key={index} className='flex gap-2 items-start rounded-lg p-3 mb-2' style={{
+                    <div key={index} className='flex gap-2 items-start rounded-lg p-3 mb-2 leading-7' style={{
                         backgroundColor: Colors.CHAT_BACKGROUND,
                     }}>
                         {!isLoaded && <Skeleton className="h-[35px] w-[35px] rounded-full" />}
                         {isLoaded && message?.role == 'user' && <Image src={user?.imageUrl!} width={35} height={35} alt="avatar" className='rounded-full' />}
+                        {loading == true && message?.role == 'ai' && <Loader2 className='h-4 w-4 animate-spin' />}
                         <h2>{message.content}</h2>
                     </div>))}
+                {loading && <div className='flex gap-2 items-start rounded-lg p-3 mb-2' style={{
+                    backgroundColor: Colors.CHAT_BACKGROUND,
+                }}>
+                    <Loader2 className='h-4 w-4 animate-spin' />
+                </div>}
             </div>
             <div className='p-5 border rounded-xl max-w-2xl w-full mt-3'
                 style={{
                     backgroundColor: Colors.BACKGROUND
                 }}>
                 <div className='flex gap-2'>
-                    <textarea placeholder={Lookup.INPUT_PLACEHOLDER} onChange={(event) => setUserInput(event.target.value)} className='outline-none border-none bg-transparent w-full !h-32 !max-h-56 resize-none' />
+                    <textarea placeholder={Lookup.INPUT_PLACEHOLDER} onChange={(event) => setUserInput(event.target.value)} value={userInput} className='outline-none border-none bg-transparent w-full !h-32 !max-h-56 resize-none' />
                     {userInput && <ArrowRight
                         onClick={() => onGenerate(userInput)}
                         className='w-10 h-10 p-2 rounded-md text-secondary-foreground bg-gradient-to-tr from-teal-500 via-cyan-500 to-sky-500 cursor-pointer' />}
